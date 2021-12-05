e067e58ddf7e84fb501fb7576f884fc19b719bd0
Yahya Alwan <95567513+alwanyah@users.noreply.github.com>
2021-12-05 17:52:02 +0100

    Create Target.cs

---
+++ Target.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+    public void TakeDamage(float amout)
+    {
+        health -= amout;
+        
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
